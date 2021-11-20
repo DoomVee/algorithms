995e4f67abb40a4f22fb1a1804075188ca934f3b
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-11-20 19:11:46 +0100

    fix: merge-sort spec 🐛
    Took 36 seconds

--- test/sorting/merge-sort.spec.ts
+++ test/sorting/merge-sort.spec.ts
@@ -2,8 +2,7 @@ import { sort } from "@/sorting/merge-sort";
 
 describe('Sorting / Merge Sort', function () {
     test('Check Normal Sorting', () => {
-        let experiment: number[] = [3, 4, 1, 8, 9, 0];
-        sort(experiment);
+        let experiment: number[] = sort([3, 4, 1, 8, 9, 0]);
         expect(experiment).toStrictEqual([0, 1, 3, 4, 8, 9]);
     });
 });
