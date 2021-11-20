e57c3b7f8d9669c5b54d16ad0a095e090b578500
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-11-20 13:08:39 +0100

    feat: sorting algorithms ✨

---
+++ src/algorithm/merge.sort.ts
@@ -0,0 +1,28 @@
+export function sort<T>(array: T[]) {
+  return mergeSort(array);
+}
+
+function mergeSort<T>(array: T[]) {
+  if(array.length <= 1) {
+    return array;
+  }
+
+  let left = mergeSort(array.slice(0, Math.floor(array.length/2)));
+  let right = mergeSort(array.slice(Math.floor(array.length/2), array.length));
+
+  return merge(left, right);
+}
+
+function merge<T>(left: T[], right: T[]) {
+  let result: T[] = [];
+
+  while(left.length > 0 && right.length > 0) {
+    if(left[0] <= right[0]) {
+      result.push(left.shift()!);
+    } else {
+      result.push(right.shift()!);
+    }
+  }
+
+  return result.concat(left).concat(right);
+}
