64b7961e50e764d639e51ddf421e3c782553a1d3
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-11-20 13:06:34 +0100

    feat: floor power of two and xor-swap ✨

--- src/math.ts
+++ src/math.ts
@@ -15,3 +15,19 @@ export function eratosthenesSieve(int: number): Array<number> {
   }
   return array;
 }
+
+export function floorPowerOfTwo(x: number) {
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x - (x >> 1);
+}
+
+export function xorSwap(arr: Array<number>, alpha: number, beta: number) {
+  arr[alpha] = arr[alpha] ^ arr[beta];
+  arr[beta] = arr[beta] ^ arr[alpha];
+  arr[alpha] = arr[alpha] ^ arr[beta];
+  return arr;
+}
