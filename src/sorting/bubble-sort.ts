f1f5b2b7d8e0adf5009924d8d46130a4e66fa56b
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-11-20 19:11:11 +0100

    feat: sorting algorithms ✨
    Took 1 hour 54 minutes

--- src/sorting/bubble-sort.ts
+++ src/sorting/bubble-sort.ts
@@ -1,4 +1,23 @@
-export function sort<T>(array: Array<T>) {
-    // TODO: Implement!
-    return null;
+export function sort<T>(arr: T[]) {
+    for(let i = 0; i < arr.length - 1; i++) {
+        for(let j = 0; j < arr.length - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                let tmp: T = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+}
+
+export function numericSort(arr: number[]) {
+    for(let i = 0; i < arr.length - 1; i++) {
+        for(let j = 0; j < arr.length - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                arr[j+1] = arr[j+1] ^ arr[j];
+                arr[j] = arr[j] ^ arr[j+1];
+                arr[j+1] = arr[j+1] ^ arr[j];
+            }
+        }
+    }
 }
